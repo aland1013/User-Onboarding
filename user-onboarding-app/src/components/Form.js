1acc830c87a777d4dc243de6aec7017d3be88acc
aland1013 <adam.land@gmail.com>
2020-04-28 16:45:01 -0400

    added state to form

--- user-onboarding-app/src/components/Form.js
+++ user-onboarding-app/src/components/Form.js
@@ -1,22 +1,63 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 
 const Form = () => {
+  const [formState, setFormState ] = useState({
+    name: '',
+    email: '',
+    password: '',
+    terms: ''
+  });
+
+  const inputChange = (e) => {
+    console.log('input changed', e.target.value);
+    e.persist();
+    const newFormData = {
+      ...formState,
+      [e.target.name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value
+    };
+    setFormState(newFormData);
+  }
+
   return (
     <form>
-      <label>
+      <label htmlFor="name">
         Name
-        <input type="text" name="name" />
+        <input 
+          type="text" 
+          name="name" 
+          id="name"
+          onChange={inputChange}
+          value={formState.name} 
+        />
       </label>
-      <label>
+      <label htmlFor="email">
         Email
-        <input type="email" name="email" />
+        <input 
+          type="email" 
+          name="email" 
+          id="email"
+          onChange={inputChange}
+          value={formState.email} 
+        />
       </label>
-      <label>
+      <label htmlFor="password">
         Password
-        <input type="text" name="password" />
+        <input 
+          type="text" 
+          name="password" 
+          id="password" 
+          onChange={inputChange}
+          value={formState.password}
+        />
       </label>
-      <label>
-        <input type="checkbox" name="terms" checked={true} />
+      <label name="terms">
+        <input 
+          type="checkbox" 
+          name="terms" 
+          id="terms" 
+          checked={formState.terms} 
+          onChange={inputChange}
+        />
         Terms & Conditions
       </label>
       <button type="submit">submit</button>
