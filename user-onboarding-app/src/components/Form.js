12567713bbb26b631a6522a221c6a5ed3554eab8
aland1013 <adam.land@gmail.com>
2020-04-28 00:06:56 -0400

    completed step 1

--- user-onboarding-app/src/components/Form.js
+++ user-onboarding-app/src/components/Form.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <form>
+      <label>
+        Name
+        <input type="text" name="name" />
+      </label>
+      <label>
+        Email
+        <input type="email" name="email" />
+      </label>
+      <label>
+        Password
+        <input type="text" name="password" />
+      </label>
+      <label>
+        <input type="checkbox" name="terms" checked={true} />
+        Terms & Conditions
+      </label>
+      <button type="submit">submit</button>
+    </form>
   );
 }
 
